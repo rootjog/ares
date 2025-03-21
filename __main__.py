ebe47a42a02b5a3ea3062fdc4d27e47eae74bcb8
Juan <joitgar@aulaestudio.es>
2025-03-21 16:46:37 +0100

    Create __main__.py

---
+++ __main__.py
@@ -0,0 +1,12 @@
+def main():
+    # Establish connection.
+
+    # Retrieve applications and services.
+
+    # Retrieve information about public exploits.
+
+    # Ask AI for commands to exploit them.
+
+    # If success then return, 
+    # else throw exception and exit.
+    ...
